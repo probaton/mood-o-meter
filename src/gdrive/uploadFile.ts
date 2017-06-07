835c2ec90f5141e60188fa49647c3c7d6d290692
Dexx <dexx.van.der.mandele@mendix.com>
2017-06-07 16:51:28 +0200

    Upload existing json file

--- src/gdrive/uploadFile.ts
+++ src/gdrive/uploadFile.ts
@@ -1,19 +1,21 @@
 import * as gApi from "googleapis";
 import * as gAuth from "google-auth-library";
 import { handleCallback, callApi } from "./gdrive";
+import { createReadStream } from "fs";
 
 callApi(uploadFile);
 
+
 function uploadFile(drive) {
+    const moodOMeterFolderId = "0B0Cu1R5HusBldjUtUkNsVmNpamc";
     drive.files.create({
         resource: {
-            parents: [ "0B0Cu1R5HusBldjUtUkNsVmNpamc" ],
-            name: "CAAFlarble",
-            mimeType: "text/plain",
+            parents: [ moodOMeterFolderId ],
+            name: "MoodOMeter1.json"
         },
         media: {
-            mimeType: "text/plain",
-            body: "Flarbler2!"
+            mimeType: "application/json",
+            body: createReadStream(process.cwd() + "/data/moodometer.json")
         }
     }, (err, res) => {
         handleCallback(err, "Uploading file failed");
